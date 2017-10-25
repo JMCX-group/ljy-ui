23c7a57a4572cde1f28861ffd6d8ac1dfe391000
liyingxuan <mvp_xuan@163.com>
2017-10-25 18:21:29 +0800

    重构banner swiper。

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,45 +2,50 @@
   <div id="banner">
     <div class="div-carousel">
 
-      <div id="myCarousel" class="carousel slide">
-        <!-- 轮播（Carousel）指标 -->
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <!-- 轮播（Carousel）项目 -->
-        <div class="carousel-inner img-style">
-          <div class="item active">
-            <img src="../assets/demo-img/banner1.jpg" class="img-responsive center-block" alt="First slide">
-          </div>
-          <div class="item">
-            <img src="../assets/demo-img/banner2.jpg" class="img-responsive center-block" alt="Second slide">
-          </div>
-          <div class="item">
-            <img src="../assets/demo-img/banner3.jpg" class="img-responsive center-block" alt="Third slide">
-          </div>
-        </div>
-        <!-- 轮播（Carousel）导航 -->
-        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-      </div>
+      <swiper :options="swiperOption">
+        <!--<swiper-slide><img :src="this.imgUrl[0]" class="img-responsive " alt=""></swiper-slide>-->
+        <swiper-slide><img src="../assets/demo-img/banner1.jpg" class="img-responsive " alt=""></swiper-slide>
+        <swiper-slide><img src="../assets/demo-img/banner2.jpg" class="img-responsive " alt=""></swiper-slide>
+        <swiper-slide><img src="../assets/demo-img/banner3.jpg" class="img-responsive " alt=""></swiper-slide>
+        <div class="swiper-pagination swiper-pagination-white" slot="pagination"></div>
+      </swiper>
 
     </div>
   </div>
 </template>
 
 <script>
+  import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
   export default {
     name: 'Banner',
+    data() {
+      return {
+        imgUrl: [
+          '../assets/demo-img/banner1.jpg',
+          '../assets/demo-img/banner2.jpg',
+          '../assets/demo-img/banner3.jpg'
+        ],
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          paginationClickable: true,
+          autoplay: 3000,
+          autoHeight: true,
+          slidesPerView: 1,
+          spaceBetween: 30,
+          loop: true
+        }
+      }
+    },
+    components: {
+      swiper,
+      swiperSlide
+    }
   }
 </script>
 
 <style>
   .div-carousel {
     margin-top: 88px;
-  }
-  .item {
-    text-align: center;
   }
 </style>
