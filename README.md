036673ee951c91c3143d5210244aafa7b2aca4c7
“liyingxuan” <mvp_xuan@163.com>
2018-08-11 16:43:16 +0800

    Update some info.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #### 辣椒旅行
+
+辣椒游项目前端，用Vue-JS做的。
+
+![Logo](./src/assets/logo.png)
+
+未完成。。。
--- src/components/My.vue
+++ src/components/My.vue
@@ -1,5 +1,38 @@
 <template>
   <div id="my">
+    <div class="container div-custom-bg-color">
+
+      <div class="row div-top-bg">
+        <div class="vertical-center div-personal-info">
+          <img class="center-block" src="../assets/logo.png"/>
+        </div>
+      </div>
+
+      <div class="row div-order-wallet"></div>
+
+      <div class="row div-function-list-1">
+        <div class="row div-function-item"></div>
+        <div class="row div-function-line center-block"></div>
+        <div class="row div-function-item"></div>
+
+        <div class="row div-function-item"></div>
+
+        <div class="row div-function-item"></div>
+
+        <div class="row div-function-item"></div>
+      </div>
+
+      <div class="row div-function-list-2">
+        <div class="row div-function-item"></div>
+      </div>
+
+      <div class="row div-function-list-3">
+        <div class="row div-function-item"></div>
+
+        <div class="row div-function-item"></div>
+      </div>
+
+    </div>
 
     <nav-bar-bottom :clicked="clickedVal"></nav-bar-bottom>
   </div>
@@ -22,5 +55,53 @@
 </script>
 
 <style>
+  .div-custom-bg-color {
+    min-height: 1300px;
+    background-color: #ebebeb;
+    margin-bottom: 150px;
+  }
 
+  .div-top-bg {
+    background: url("../assets/demo-img/banner1.jpg") fixed center no-repeat;
+    background-size: cover;
+    padding-bottom: 46%;
+  }
+
+  /* div 水平垂直居中 */
+  .vertical-center{
+    position: relative;
+    /*top: 50%;*/
+    left: 50%;
+    transform: translate(-50%, 75%);
+  }
+
+  .div-personal-info img {
+    height: 166px;
+    width: 166px;
+  }
+
+  .div-order-wallet {
+    height: 130px;
+    background-color: white;
+  }
+
+  .div-function-list-1,
+  .div-function-list-2,
+  .div-function-list-3 {
+    margin-top: 24px;
+  }
+  .div-function-item {
+    height: 98px;
+    background-color: white;
+    margin: 0;
+  }
+  .div-function-line {
+    height: 2px;
+    background-color: #ebebeb;
+    margin-left: 39px;
+    margin-right: 39px;
+  }
+  .div-function-list-1 {
+
+  }
 </style>
